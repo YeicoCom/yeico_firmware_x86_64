7c5a87b8e5db9a1c47095f59ac212d1b26f26b7d
Samuel Ventura <samuel.ventura@yeico.com>
2025-12-14 18:51:29 -0600

    Fw root partion to 512MB.

---
+++ shell.sh
@@ -0,0 +1,5 @@
+
+~/yeico_firmware_x86_64/deps/nerves_system_br/create-build.sh \
+    ~/yeico_firmware_x86_64/nerves_defconfig \
+    ~/yeico_firmware_x86_64/.nerves/artifacts/yeico_firmware_x86_64-portable-1.32.0 >/dev/null && \
+    cd ~/yeico_firmware_x86_64/.nerves/artifacts/yeico_firmware_x86_64-portable-1.32.0
