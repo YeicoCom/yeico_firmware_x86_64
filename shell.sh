c0a5b0fd24b1c36e6605c31cfa73ff205e70591d
Samuel Ventura <samuel.ventura@yeico.com>
2026-01-16 22:06:31 -0600

    Shell script marks patched packages.

--- shell.sh
+++ shell.sh
@@ -8,20 +8,25 @@ case "$1" in
     deps/nerves_system_br/create-build.sh nerves_defconfig .nerves/artifacts/yeico_firmware_x86_64-portable-1.32.0
     cd .nerves/artifacts/yeico_firmware_x86_64-portable-1.32.0
     echo Running within an screen session
-    $SELF patch >> ${SELF%.*}.log
+    $SELF patch | tee ${SELF%.*}.log
     bash
     ;;
     patch)
+    counter=0
     for p in $(dirname $SELF)/patches/*.patch; do
         echo $p
         array=(${p//-/ })
         package=${array[1]}
         version=${array[2]}
-        ls build/$package-$version
-        (cd build/$package-$version && (patch -p1 < $p))
-        make $package-rebuild
+        dir=build/$package-$version
+        [ -f $dir/.yeico_patched ] || echo $dir
+        [ -f $dir/.yeico_patched ] || (cd build/$package-$version && (patch -p1 < $p))
+        [ -f $dir/.yeico_patched ] || make $package-rebuild
+        [ -f $dir/.yeico_patched ] || counter=$((counter + 1))
+	[ -f $dir/.yeico_patched ] || touch $dir/.yeico_patched
     done
-    make
+    echo Total patched: $counter
+    [ $counter -gt 0 ] && make
     ;;
     *)
     cd $(dirname $SELF)
